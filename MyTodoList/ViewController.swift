cb4cb2292636aec2ee3ea69cb3d51256713c49d6
Rodrigo Vargas <rvargas@cooperativa.cl>
2016-05-27 15:36:13 -0400

    Primer Comiteo de proyecto en blanco

---
+++ MyTodoList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTodoList
+//
+//  Created by Innovación y Desarrollo on 27-05-16.
+//  Copyright © 2016 Innovación y Desarrollo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
